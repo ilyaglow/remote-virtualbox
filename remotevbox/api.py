604975d118da180d1b54607f2b5950bce0cce685
Ilya Glotov <ilya.glotov@gmail.com>
2017-11-14 18:43:26 +0300

    Add connect API function

---
+++ remotevbox/api.py
@@ -0,0 +1,14 @@
+"""
+Main API module
+"""
+
+from .vbox import IVirtualBox
+
+
+def connect(location, user, password):
+    """Connects and returns IVirtualBox object"""
+    return IVirtualBox(location, user, password)
+
+
+def disconnect():
+    pass
