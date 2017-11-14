b41a77ed36910a2377a55499ce3839e2774b12b3
Ilya Glotov <ilya.glotov@gmail.com>
2017-11-14 18:44:56 +0300

    Add IWebsession_manager class

---
+++ remotevbox/websession_manager.py
@@ -0,0 +1,33 @@
+"""
+IWebsession_Manager binding
+"""
+
+import logging
+import zeep.exceptions
+
+
+class IWebsessionManager(object):
+    """
+    IWebsessionManager retrieves handle and current session
+    """
+    def __init__(self, service, user, password):
+        self.service = service
+        self.handle = self.login(user, password)
+
+        if self.handle:
+            self.session = self.get_session(self.handle)
+
+    def get_session(self, handle):
+        """Retrieves current SessionObject"""
+        return self.service.IWebsessionManager_getSessionObject(handle)
+
+    def login(self, user, password):
+        """Use IWebsessionManager to login and exit if credentials are false
+
+        :param user: username (VBOX_USER from /etc/default/virtualbox)
+        :param password: unix password of the supplied user on VirtualBox host
+        """
+        try:
+            return self.service.IWebsessionManager_logon(user, password)
+        except zeep.exceptions.Fault:
+            logging.error("Wrong credentials supplied")
