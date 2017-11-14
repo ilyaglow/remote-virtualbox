03ad021f8015686f3afee3f2bd96aec163757cef
Ilya Glotov <ilya.glotov@gmail.com>
2017-11-14 18:44:15 +0300

    Add simple IMachine class

--- remotevbox/machine.py
+++ remotevbox/machine.py
@@ -1,2 +1,26 @@
-class IMachine():
-    def __init__(self, vbox_handle, id): 
+"""
+IMachine class represents IMachine object
+"""
+
+
+class IMachine(object):
+    """IMachine constructs object with service, manager and id"""
+    def __init__(self, service, manager, mid):
+        self.mid = mid
+        self.service = service
+        self.manager = manager
+        self.session = self.manager.get_session(self.mid)
+
+    def launch(self):
+        """Launches stopped or powered off machine
+        Returns IProgress"""
+        progress = self.service.IMachine_launchVMProcess(self.mid,
+                                                         self.session,
+                                                         "nogui",
+                                                         "")
+        return progress
+
+    def stop(self):
+        """Save state of running machine"""
+        progress = self.service.IMachine_saveState(self.mid)
+        return progress
