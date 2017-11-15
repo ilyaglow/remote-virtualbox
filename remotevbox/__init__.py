023b8dabfb05f66132d4e102060a80038044b168
Ilya Glotov <ilya.glotov@gmail.com>
2017-11-15 19:37:40 +0300

    Add more detailed example of usage

--- remotevbox/__init__.py
+++ remotevbox/__init__.py
@@ -6,9 +6,13 @@ Modern little package to do simple things with VirtualBox remotely
 
 Usage:
     >>> import remotevbox
-    >>> client = remotevbox.connect("http://127.0.0.1:18083", "vbox", "yourpassphrase")
-    >>> client.get_version()
+    >>> vbox = remotevbox.connect("http://127.0.0.1:18083", "vbox", "yourpassphrase")
+    >>> vbox.get_version()
     '5.1.30'
+    >>> machine = vbox.get_machine("Windows10")
+    >>> machine.launch()
+    >>> machine.save()
+    >>> vbox.disconnect()
 """
 
-from  .api import connect, disconnect
+from  .api import connect
