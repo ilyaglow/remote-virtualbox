2e270eb712f008bbdaa3f7db963a6e2993ae2245
Ilya Glotov <ilya.glotov@gmail.com>
2018-06-29 16:38:59 +0300

    Accept omitted user and password fields for connect

--- remotevbox/api.py
+++ remotevbox/api.py
@@ -5,6 +5,6 @@ Main API module
 from .vbox import IVirtualBox
 
 
-def connect(location, user, password):
+def connect(location, user="", password=""):
     """Connects and returns IVirtualBox object"""
     return IVirtualBox(location, user, password)
